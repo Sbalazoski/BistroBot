dae1b367cba5e5c72411897fe8a7cb46810a8dee
[dyad] <git@dyad.sh>
2025-11-16 18:46:30 -0600

    Reverted all changes back to version ffd1e17a07fe1828e2f545ba3df5e46fabbbc130

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,10 @@
-"use client";
-
-import React from "react";
-import AuthPage from "./AuthPage";
-import MadeWithDyad from "@/components/MadeWithDyad";
-import BistroBotPatternOverlay from "@/components/BistroBotPatternOverlay"; // Import the new component
+import { MadeWithDyad } from "@/components/made-with-dyad";
+import AuthPage from "./AuthPage"; // Import AuthPage
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-orange-300 via-rose-300 to-purple-300 dark:from-indigo-950 dark:via-purple-950 dark:to-pink-950 relative">
-      {/* Pattern overlay */}
-      <BistroBotPatternOverlay />
-
-      <main className="flex-grow flex items-center justify-center p-4 relative z-20">
+    <div className="min-h-screen flex flex-col bg-gradient-to-br from-orange-300 via-rose-300 to-purple-300 dark:from-indigo-950 dark:via-purple-950 dark:to-pink-950">
+      <main className="flex-grow flex items-center justify-center p-4">
         <AuthPage /> {/* Render the AuthPage component directly */}
       </main>
       <MadeWithDyad />
