b85f6b44023d2d04654a5fbea19005a51000bf0f
[dyad] <git@dyad.sh>
2025-11-16 18:52:39 -0600

    [dyad] wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,7 @@ import { MadeWithDyad } from "@/components/made-with-dyad";
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-950 relative"> {/* Clean, subtle background */}
+    <div className="min-h-screen flex flex-col bg-gradient-to-br from-purple-400 via-pink-500 to-red-500 dark:from-indigo-900 dark:via-purple-900 dark:to-pink-900 relative"> {/* Dynamic gradient background */}
       <main className="flex-grow flex items-center justify-center p-4 relative z-20">
         <AuthPage /> {/* Render the AuthPage component directly */}
       </main>
